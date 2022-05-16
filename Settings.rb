5cbe81b761c5001d3e2febccdcc7fe26d223318e
Niu25 <105356920+Niu25@users.noreply.github.com>
2022-05-16 15:47:29 +0200

    Add files via upload

---
+++ Settings.rb
@@ -0,0 +1,12 @@
+class VirtualBoxSettings
+  Vb_gui = true
+  Vb_name = 'VBoxBionic'
+  Vb_cpus = 4
+  Vb_cpuexecutioncap = 90
+  Vb_monitors = 1
+  Vb_memory = 2048
+  Vb_vram = 256
+  Vb_proxy = 'http://dc-proxy-rat.net.vodafone.com:8080/'
+  Vb_no_proxy = 'localhost,127.0.0.1,.kabeldeutschland.de,.vf-aws.de,.dxl-vf.de,.coma-vf.de'
+  Vb_publicNetwork = false
+end
